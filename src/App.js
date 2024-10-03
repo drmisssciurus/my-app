a1931828ff24be5e7a57631798217d3a03d99aa7
Alina <ms.sciurus@gmail.com>
2024-10-03 17:57:39 +0300

    events in react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-const Header = () => {
-  return <h2>Hello There!</h2>
-}
-
-const Field = () => {
-  const styleField = {
-    width: '300px'
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      position: ''
+    }
   }
-  return <input 
-          placeholder="Type Here" 
-          type="text" 
-          style={styleField} />
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    const {position, years} = this.state;
+
+    console.log(this);
+    
+    return (
+      <div>
+        <button onClick={this.nextYear}>+++</button>
+        <h1>My name is {name}, surname - {surname}, 
+            age - {years},
+            position - {position}</h1>
+        <a href={link}>My Profile</a>
+        <form>
+          <span>Put Profession</span>
+          <input type="text" onInput={this.commitInputChanges}/>
+        </form>
+      </div>
+    )
+  }
 }
 
-function Btn() {
-  return <button className="btn">HEHE</button>
-}
+// const Header = () => {
+//   return <h2>Hello There!</h2>
+// }
+
+// const Field = () => {
+//   const styleField = {
+//     width: '300px'
+//   }
+//   return <input 
+//           placeholder="Type Here" 
+//           type="text" 
+//           style={styleField} />
+// }
+
+// function Btn() {
+//   return <button className="btn">HEHE</button>
+// }
+
 function App() {
   return (
-  <div className="App">
-    <Header />
-    <Field />
-    <Btn />
-  </div>
+    <div className="App">
+      <WhoAmI name="Alina" surname="Levintas" link="google.com" />  
+      <WhoAmI name="Aragorn" surname="notForgotten" link="numenor.com" />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -16,5 +16,5 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App/>,
-  document.getElementById('root')
 );
+
